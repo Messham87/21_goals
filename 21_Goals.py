933ca8733a77715ed08acd4e2a50bab9124553ee
Stephen J Messham <Ste@Stephens-MacBook-Pro.local>
2023-12-17 13:33:13 +0000

    minor tweaks

--- 21_Goals.py
+++ 21_Goals.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
+import datetime
+import os
+filePath = "./data/masterdf.parquet"
 st.set_page_config(layout="wide")
-goals_data = pd.read_parquet("./data/masterdf.parquet")
+goals_data = pd.read_parquet(filePath)
 preds = pd.DataFrame({
     "LM" : ["Bruno Fernandes", "V. van Dijk", "Rúben Dias", "Rodri"], 
     "SS" : ["Bruno Fernandes", "V. van Dijk", "T. Alexander-Arnold", "S. March"], 
@@ -21,7 +24,7 @@ with SM:
     df.columns = ["Player", "Goals"]
     st.header(player)
     st.header(df["Goals"].sum().astype(int))
-    st.dataframe(df, hide_index = True)
+    st.dataframe(df.sort_values("Goals", ascending = False), hide_index = True)
 
 with SS:
     player = "SS"   
@@ -29,7 +32,7 @@ with SS:
     df.columns = ["Player", "Goals"]
     st.header(player)
     st.header(df["Goals"].sum().astype(int))
-    st.dataframe(df, hide_index = True)
+    st.dataframe(df.sort_values("Goals", ascending = False), hide_index = True)
 
 with DH:
     player = "DH"   
@@ -37,7 +40,7 @@ with DH:
     df.columns = ["Player", "Goals"]
     st.header(player)
     st.header(df["Goals"].sum().astype(int))
-    st.dataframe(df, hide_index = True) 
+    st.dataframe(df.sort_values("Goals", ascending = False), hide_index = True)
 
 with LM:
     player = "LM"   
@@ -45,12 +48,14 @@ with LM:
     df.columns = ["Player", "Goals"]
     st.header(player)
     st.header(df["Goals"].sum().astype(int))
-    st.dataframe(df, hide_index = True)
+    st.dataframe(df.sort_values("Goals", ascending = False), hide_index = True)
      
 with JM:
     player = "JM"   
     df = goals_data[(goals_data["player.name"].isin(preds[player]))&(goals_data["league.id"]==39)][["player.name", "goals.total"]]    
     df.columns = ["Player", "Goals"]
     st.header(player)
-    st.header(df["Goals"].sum())
-    st.dataframe(df, hide_index = True)
+    st.header(df["Goals"].sum().astype(int))
+    st.dataframe(df.sort_values("Goals", ascending = False), hide_index = True)
+
+st.text(f"Last refresh: {datetime.datetime.fromtimestamp(os.path.getmtime(filePath))}")
